9ff01fa87d4f435ca17317f146622cd6a54e8ddc
David C. Bishop <david@davidbishop.org>
2012-08-04 08:20:27 +1000

    Stop initializing the same Initilizer twice. Check for VAO support.

--- src/Initializer.cpp
+++ src/Initializer.cpp
@@ -1,24 +1,22 @@
+#include <stdexcept>
 #include "Initializer.hpp"
 
+#include "OpenGL.hpp"
 #include "Cube.hpp"
 #include "TextureQuad.hpp"
 
 namespace dglw {
 
-/*Initializer global_initializer_g;
-global_initializer_g.setGlobalDefaults();
-
-void init() {
-   if(!global_initializer_g.isInitialized()) {
-      global_initializer_g.initialize();
-   }
-}*/
-
 Initializer::Initializer() {
-   // Initialize cube
+   is_initialized_ = false;
 }
 
 void Initializer::initialize() {
+   if(is_initialized_) {
+      DEBUG_M("Already initialized this list...");
+      return;
+   }
+
    for(VoidFunction vf : init_list_) {
       if(vf) {
          vf();
@@ -34,16 +32,21 @@ bool Initializer::isInitialized() {
 }
 
 void Initializer::addInitializeFunction(VoidFunction vf) {
+   if(is_initialized_) {
+      std::string error_msg("Tried to add stuff to an initializer after it was initilized.");
+      ERROR("%s", error_msg.c_str());
+      throw std::runtime_error(error_msg);
+   }
    init_list_.push_back(vf);
 }
 
-//void Initializer::setGlobalDefaults() {
-   //addInitializeFunction(glewInit());
-   //addInitializeFunction(Cube::init());
-//}
-
 void initialize() {
    DEBUG_M("Initializing dglw...");
+   logGLError();
+   if(glGenVertexArrays == nullptr) {
+      ERROR("Vertex Array Objects not supported...");
+      return;
+   }
    Cube::initialize();
    TextureQuad::initialize();
 }
